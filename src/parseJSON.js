c5a73ddbb31fd8307d2f8e0d03b9e467bddf49f7
Joshua Booth <joshua.n.booth@gmail.com>
2021-09-20 12:25:55 -0700

    Match on object type

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,18 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
+  var initValue = json[0];
   // your code goes here
+  console.log(JSON.parse(json));
+
+  if (initValue === '[') {
+    //Array
+    parseJSON(json.substring(1, json.length));
+  } else if(initValue === '{') {
+    parseJSON(json.substring(1, json.length));
+    //Object
+  } else if(initValue === ',') {
+
+  }
+
 };
