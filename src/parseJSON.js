dcb86010d2b489618ec2072c669ac2a6e9ca72a5
capturetheworld <ian@iansoohoo.me>
2021-09-20 21:35:15 -0700

    finished

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,80 +3,72 @@
 
 // but you're not, so you'll write it from scratch:
 
-// parseJSON('null')
-// parseJSON('[null]')
-var debugg;
 var parseJSON = function(json) {
-  debugg = json;
   var position = 0;
-  var scan = function() {
-    while (' \n'.indexOf(json[position]) !== -1) {
+  var skipWhitespace = function() {
+    while (position < json.length && ' \n\r\t'.indexOf(json[position]) !== -1) {
       position++;
     }
-    // console.log(json.substring(position));
+  };
+  var scan = function() {
+    skipWhitespace();
     var initValue = json[position];
     position++;
 
     if (initValue === '[') {
       let output = [];
-      console.log("array");
       //Array
-      while (position < json.length){
+      while (position < json.length) {
+        skipWhitespace();
         if (json[position] === ']') {
           position++;
           return output;
         }
         var element = scan();
         output.push(element);
-        while (position < json.length && (json[position] === ',' || json[position] === ' ')) {
+        skipWhitespace();
+        if (json[position] === ',') {
           position++;
         }
       }
       throw new SyntaxError('unterminated array');
 
 
-    } else if(initValue === '{') {
+    } else if (initValue === '{') {
       let output = {};
-      //object
-      // console.log("object");
-      while (position < json.length){
+      while (position < json.length) {
+        skipWhitespace();
+
         if (json[position] === '}') {
-          // console.log("debug1");
-          console.log(json[position]);
           position++;
           return output;
         }
 
         var fieldName = scan();
 
-        while (position < json.length && ': \n'.indexOf(json[position]) !== -1) {
-          position++;
+        skipWhitespace();
+
+        if (json[position] !== ':') {
+          throw new SyntaxError('expected :');
         }
+        position++;
+
+        skipWhitespace();
 
         output[fieldName] = scan();
-
-        while (position < json.length && ', \n'.indexOf(json[position]) !== -1) {
+        skipWhitespace();
+        if (json[position] === ',') {
           position++;
         }
       }
       throw new SyntaxError('unterminated object');
-
-
-
-    } // "[\"\\\\\\\"\\\"a\\\"\"]" > Array ["\""a""]
-    // parseJSON('"\""') >>> unexpected string in JSON at position 2
-
-
-  // >> ["\""a""]
-    else if(initValue === '-' || initValue >= '0' && initValue <= '9'){
+    } else if (initValue === '-' || initValue >= '0' && initValue <= '9') {
       var startOfNumber = position - 1;
       while (position < json.length && (json[position] === '.' || json[position] >= '0' && json[position] <= '9')) {
         position++;
       }
       return Number(json.slice(startOfNumber, position));
-    }
-
-    else if(initValue === '"') {
+    } else if (initValue === '"') {
       var startOfString = position;
       var wasEscaped = false;
 
@@ -84,34 +76,31 @@ var parseJSON = function(json) {
       while (position < json.length) {
         if (json[position] === '\\') {
           position++;
-        } else if(json[position] === '"') {
+        } else if (json[position] === '"') {
           position++;
           return output;
         }
         output += json[position];
         position++;
       }
-      throw new SyntaxError("unterminated string");
-    }
-
-    else if (json.substring(position - 1, position+4).toLowerCase() === "false" ) {
+      throw new SyntaxError('unterminated string');
+    } else if (json.substring(position - 1, position + 4).toLowerCase() === 'false' ) {
       position += 4;
       return false;
-    }
-    else if (json.substring(position - 1, position+3).toLowerCase() === "true" ) {
+    } else if (json.substring(position - 1, position + 3).toLowerCase() === 'true' ) {
       position += 3;
       return true;
-    }
-    else if (json.substring(position - 1, position+3).toLowerCase() === "null" ) {
+    } else if (json.substring(position - 1, position + 3).toLowerCase() === 'null' ) {
       position += 3;
       return null;
+    } else {
+      skipWhitespace();
+      if (position < json.length) {
+        throw new SyntaxError('leftover tokens: ' + json.substring(position));
+      }
     }
-    else {
-      console.log("Help! Something went wrong!", json.substring(position));
-    }
-  }
+  };
 
   return scan(json);
-
 };
 
